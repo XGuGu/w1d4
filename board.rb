8663816ddaa1e03785766ee2a25348daf6489425
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-26 17:35:54 -0400

    final!!!! fix!!!!!!

--- board.rb
+++ board.rb
@@ -4,17 +4,17 @@ class Board
   attr_reader :grid
 
   def self.empty_grid
-    @grid = Array.new(9) do
+    grid = Array.new(9) do
       Array.new(9) { Tile.new(0) }
     end
     grid
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map { |char| Integer(char) }
+      nums.map { |num| Tile.new(num) }
     end
 
     self.new(tiles)
@@ -32,15 +32,15 @@ class Board
   def []=(pos, value)
     x, y = pos
     tile = grid[x][y]
-    tile.value = new_value
+    tile.value = value
   end
 
   def columns
-    rows.transpose!
+    rows.transpose
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  #{(0..8).to_a.join(" ")}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -51,7 +51,10 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
+  def rows
+    grid
+  end
+  # alias_method :rows, :size
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
@@ -61,7 +64,7 @@ class Board
 
   def solved_set?(tiles)
     nums = tiles.map(&:value)
-    nums.sort == (1..9)
+    nums.sort == (1..9).to_a
   end
 
   def square(idx)
@@ -79,7 +82,7 @@ class Board
   end
 
   def squares
-    (0..8).to_a.each { |i| square(i) }
+    (0..8).to_a.map { |i| square(i) }
   end
 
 end
